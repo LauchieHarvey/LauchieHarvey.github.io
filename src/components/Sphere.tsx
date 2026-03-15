b339f3573aa6ce79371964a8c4c047d5b923d9d3
Lauchie Harvey <lauchie.harvey@truii.com>
2026-03-15 10:20:13 +1000

    Updated sphere to better model. some style improvements

--- src/components/Sphere.tsx
+++ src/components/Sphere.tsx
@@ -1,22 +1,44 @@
 import { useGLTF } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
 import { type Mesh } from "three";
 
 const SphereMesh = () => {
   const meshRef = useRef<Mesh | null>(null);
+  const { camera } = useThree();
+  const [scrollY, setScrollY] = useState<number>(0);
 
   const { scene } = useGLTF("/wordCloudSphere.glb");
 
+  const minCameraY = -2;
+  const maxCameraY = 2;
+  const scrollFactor = 0.005;
+
+  useEffect(() => {
+    const onScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   useFrame((state, delta) => {
+    // Rotate sphere around Y axis.
     if (meshRef.current != null) {
       meshRef.current.rotateY(-delta * 0.1);
     }
+
+    // Update camera Y position based on scroll.
+    const newCameraY = maxCameraY - scrollY * scrollFactor;
+    camera.position.y = Math.min(maxCameraY, Math.max(newCameraY, minCameraY));
+    camera.lookAt(0, 0, 0);
   });
 
   return (
     <group scale={0.1}>
-      <primitive object={scene} ref={meshRef} />
+      <primitive object={scene} ref={meshRef} color="red" />
     </group>
   );
 };
@@ -25,11 +47,8 @@ const Sphere = () => {
   return (
     <Canvas
       style={{ width: "100%", height: "100%" }}
-      camera={{ position: [9, 0, 0] }}
+      camera={{ position: [8, 2, 0] }}
     >
-      <ambientLight intensity={0.7} />
-      <directionalLight position={[9, 9, 2]} />
-
       <SphereMesh />
     </Canvas>
   );
